98cd5d31b9f41e8769c657cd7f62a97c155e2dd1
Ampfy <flavio@ampfy.com>
2018-02-16 18:30:01 -0200

    Aula-08

--- aula08/01valor.php
+++ aula08/01valor.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
 	<meta charset="UTF-8">
 	<title>Operações</title>
-	<link rel="stylesheet" href="../aula08/_css/estilo.css">
+	<link rel="stylesheet" href="../aula05/_css/estilo.css">
 </head>
 <body>
 	<div>
 		<?php
-			
-		 ?>
+			$valor = $_GET["v"];
+			echo "O Valor enviado foi $valor<br>";
+			echo "A Raiz Quadrada do valor é ". number_format(sqrt($valor), 2);
+		 ?><br>
+		 <a href="01exercicio.html">Voltar</a>
 	 </div>
 </body>
 </html>
